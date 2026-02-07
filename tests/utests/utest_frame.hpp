7559f5f0f5ab5e8829c7032870d185abe0fa46e0
rughar <mbasovnik@gmail.com>
2026-02-08 00:00:33 +0100

    U test format update

--- tests/utests/utest_frame.hpp
+++ tests/utests/utest_frame.hpp
@@ -4,23 +4,49 @@
 
 namespace utest
 {
+  struct test_counter
+  {
+    size_t total = 0;
+    size_t failed = 0;
+
+    void operator+=(size_t result)
+    {
+      ++total;
+      if (result != 0)
+        ++failed;
+    };
+
+    void write_summary() const
+    {
+      std::cout << "\033[33mTest summary\033[0m: " << total - failed << " passed, " << failed << " failed, out of " << total << " tests.\n";
+    }
+  };
+
+  void write_category(const char *category_name)
+  {
+    std::cout << "\033[34m" << category_name << "\033[0m\n";
+  }
+
   template <class TestFn>
   int run(TestFn &&test_func, const char *test_name) noexcept
   {
     try
     {
       test_func();
-      std::cout << "\033[32mPASSED\033[0m: " << test_name << "\n";
+      if (*test_name)
+        std::cout << "\033[32mPASSED\033[0m: " << test_name << "\n";
       return 0;
     }
     catch (const std::exception &e)
     {
-      std::cerr << "\033[31mFAILED\033[0m: " << test_name << " - " << e.what() << "\n";
+      if (*test_name)
+        std::cerr << "\033[31mFAILED\033[0m: " << test_name << " - " << e.what() << "\n";
       return 1;
     }
     catch (...)
     {
-      std::cerr << "\033[31mFAILED\033[0m: " << test_name << " - " << "Unknown exception\n";
+      if (*test_name)
+        std::cerr << "\033[31mFAILED\033[0m: " << test_name << " - " << "Unknown exception\n";
       return 1;
     }
   }
--- tests/utests/utest_run.cpp
+++ tests/utests/utest_run.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 #include <utest_frame.hpp>
-
 #include <ling_test.hpp>
 
 void test_smoke()
 {
+  if (1 + 1 != 2)
+    throw std::runtime_error("math is broken");
+
+  auto must_throw = []()
+  { throw std::runtime_error("expected failure"); };
+  
+  if (utest::run(must_throw, "") == 0)
+    throw std::runtime_error("run() did not catch the exception");
 }
 
 int main()
 {
-  int failed = 0;
+  utest::test_counter tc;
+  utest::write_category("smoke");
 
-  if (utest::run(test_smoke, "utest smoke"))
+  tc += utest::run(test_smoke, "utest smoke");
+  if (tc.failed)
     return 1;
 
-  failed += utest::run(test_dot_product, "ling/dot_product");
-  failed += utest::run(test_spectral_radius_estimate, "ling/spectral_radius_estimate");
-  failed += utest::run(test_solve_opt, "ling/solve_opt");
-  failed += utest::run(test_remove_tangent_components, "ling/remove_tangent_components");
+  utest::write_category("math::ling");
 
-  return failed;
+  tc += utest::run(test_dot_product, "dot_product");
+  tc += utest::run(test_spectral_radius_estimate, "spectral_radius_estimate");
+  tc += utest::run(test_solve_opt, "solve_opt");
+  tc += utest::run(test_remove_tangent_components, "remove_tangent_components");
+
+  tc.write_summary();
+
+  return tc.failed ? 1 : 0;
 }
